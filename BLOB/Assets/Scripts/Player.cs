08f425f431aa5a8b2ed3ed8e57aaad39c1dc5d92
Arsalan Bin Najeeb <abnajeeb@knox.edu>
2019-02-09 15:39:36 -0600

    Added Camera Movement

--- BLOB/Assets/Scripts/Player.cs
+++ BLOB/Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update 
     void Start()
     {
-        transform.position = new Vector3(-7.64f, -3.45f, 0);
+        //transform.position = new Vector3(-7.64f, -3.45f, 0);
     }
 
     // Update is called once per frame 
