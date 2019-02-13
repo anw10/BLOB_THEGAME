a2580183223cdd98afceae996ad4191b5be51595
Peter <pcdriscoll@gmail.com>
2019-02-12 20:33:02 -0600

    Laid groundwork for UI
    More to come tonight

---
+++ BLOB/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver = true;
+    public GameObject player;
+
+    private UI_Manager _uiManager;
+
+    private void Start()
+    {
+        _uiManager = GameObject.Find("Canvas").GetComponent<UI_Manager>();
+        _uiManager.HideOptionsScreen();
+    }
+    
+    //if game over == true
+    //if space key pressed
+    //spawn player
+    //gameOver false
+
+    void Update()
+    {
+        if (gameOver == true)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Instantiate(player, Vector3.zero, Quaternion.identity);
+                gameOver = false;
+                _uiManager.HideTitleScreen();
+            }
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                _uiManager.ShowOptionsScreen();
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                _uiManager.HideOptionsScreen();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Application.Quit();
+                Debug.Log("Ordered to End Application.");
+            }
+        }
+
+    }
+}
---
+++ BLOB/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    public GameObject Title;
+    public GameObject Play_Button;
+    public GameObject Options_Button;
+
+    public GameObject Options_Screen;
+
+
+    public void UpdateScore()
+    {
+
+    }
+
+    public void ShowTitleScreen()
+    {
+        Title.SetActive(true);
+        Play_Button.SetActive(true);
+        Options_Button.SetActive(true);
+    }
+
+    public void HideTitleScreen()
+    {
+        Title.SetActive(false);
+        Play_Button.SetActive(false);
+        Options_Button.SetActive(false);
+    }
+
+    public void ShowOptionsScreen()
+    {
+        Options_Screen.SetActive(true);
+    }
+
+    public void HideOptionsScreen()
+    {
+        Options_Screen.SetActive(false);
+    }
+}
