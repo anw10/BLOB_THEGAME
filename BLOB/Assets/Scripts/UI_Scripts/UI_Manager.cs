07fd46c1af99ce7bd4e8d042babfdaace52d6a29
Peter <pcdriscoll@gmail.com>
2019-02-26 18:47:02 -0600

    Tutorial, Credits, and Score
    Having small issues I'll try to deal with before class tomorrow. The new menus system always opens the tutorial page first no matter which button you press then works fine after that. Score font is bad because I cant use TextMeshPro with the current script. Should be minor fixes.

--- BLOB/Assets/Scripts/UI_Scripts/UI_Manager.cs
+++ BLOB/Assets/Scripts/UI_Scripts/UI_Manager.cs
@@ -7,9 +7,19 @@ public class UI_Manager : MonoBehaviour
     public GameObject Background;
     public GameObject Pause_Menu;
     public GameObject Settings_Menu;
+    public GameObject Credits_Menu;
+    public GameObject Tutorial_Menu;
+
+    private void Start()
+    {
+        Background.SetActive(true);
+        Settings_Menu.SetActive(false);
+        Credits_Menu.SetActive(false);
+        Tutorial_Menu.SetActive(false);
+    }
 
 
-    public void UpdateScore()
+        public void UpdateScore()
     {
 
     }
@@ -38,6 +48,32 @@ public class UI_Manager : MonoBehaviour
     {
         Pause_Menu.SetActive(false);
         Time.timeScale = 1;
+    }
+
+
+    public void OpenCredits()
+    {
+        Credits_Menu.SetActive(true);
+        Background.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        Credits_Menu.SetActive(false);
+        Background.SetActive(true);
+    }
+
+
+    public void OpenTutorial()
+    {
+        Tutorial_Menu.SetActive(true);
+        Background.SetActive(false);
+    }
+
+    public void CloseTutorial()
+    {
+        Tutorial_Menu.SetActive(false);
+        Background.SetActive(true);
     }
 
 
