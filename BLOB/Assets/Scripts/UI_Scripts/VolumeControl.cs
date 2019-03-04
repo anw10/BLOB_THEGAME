0531b5c13ed94bd9076e2ef0b27d26295e133eba
Peter <pcdriscoll@gmail.com>
2019-03-03 18:25:23 -0600

    Made change for Git

---
+++ BLOB/Assets/Scripts/UI_Scripts/VolumeControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public Slider slider;
+    float setVolume = 1.0f;
+
+    void Start()
+    {
+        slider.value = setVolume;
+    }
+    void Update ()
+    {
+        AudioListener.volume = setVolume;
+        setVolume = slider.value;
+        //made changes for git
+    }
+    
+}
